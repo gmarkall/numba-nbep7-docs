0cb77067cdf137d9644360b9efa79d9a902f02d5
Graham Markall <gmarkall@nvidia.com>
2020-03-06 23:01:30 +0000

    Add NBEP 7 document

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.49.0dev0+668.g5cf6befb1',
+    VERSION: '0.49.0dev0+671.g712bb69ad',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
